ae925cb5b462d1b8ecbb93e61f91457edfa3e4a1
Tejas Parikh <t.parikh@northeastern.edu>
2018-10-10 22:26:11 -0400

    appspec

---
+++ infrastructure/aws/codedeploy/afterInstall.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo systemctl stop tomcat.service
+
+sudo rm -rf /opt/tomcat/webapps/docs  /opt/tomcat/webapps/examples /opt/tomcat/webapps/host-manager  /opt/tomcat/webapps/manager /opt/tomcat/webapps/ROOT
+
+sudo chown tomcat:tomcat /opt/tomcat/webapps/ROOT.war
+
+# cleanup log files
+sudo rm -rf /opt/tomcat/logs
